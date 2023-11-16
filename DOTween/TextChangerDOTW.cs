3e794d4dfc62a077030d41ad10d32ff89c54d09a
Pove1iteL <140903532+Pove1iteL@users.noreply.github.com>
2023-11-16 17:13:44 +0700

    Add files via upload

--- DOTween/TextChangerDOTW.cs
+++ DOTween/TextChangerDOTW.cs
@@ -5,16 +5,17 @@ using UnityEngine.UI;
 public class TextChangerDOTW : MonoBehaviour
 {
     [SerializeField] private Text _text;
-    [SerializeField] private Text _text1;
-    [SerializeField] private Text _text2;
 
     private void Start()
     {
-        _text.DOText("Текст Поменен", 3f).SetLoops(-1,LoopType.Yoyo);  
-        
-        _text1.DOText(" Дополнил", 3).SetRelative().SetLoops(-1, LoopType.Yoyo);
-        _text1.DOColor(Color.black, 3).SetLoops(-1, LoopType.Yoyo);
+        Sequence sequence = DOTween.Sequence();
 
-        _text2.DOText("Перебор Текста", 3, true, ScrambleMode.All).SetLoops(-1, LoopType.Yoyo);
+        sequence.Append(_text.DOText("Текст Поменен", 3f));
+        sequence.Append(_text.DOText(" Дополнил", 3).SetRelative());
+        sequence.Append(_text.DOColor(Color.black, 3));
+        sequence.Append(_text.DOColor(Color.black, 3));
+        sequence.Append(_text.DOText("Перебор Текста", 3, true, ScrambleMode.All));
+
+        sequence.SetLoops(-1, LoopType.Yoyo);
     }
 }
