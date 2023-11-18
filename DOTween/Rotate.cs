0a3cf8c57cabb3ae01da16d61528c4db10e5027f
Pove1iteL <140903532+Pove1iteL@users.noreply.github.com>
2023-11-18 11:27:54 +0700

    Add files via upload

--- DOTween/Rotate.cs
+++ DOTween/Rotate.cs
@@ -4,9 +4,9 @@ using DG.Tweening;
 public class Rotate : MonoBehaviour
 {
     [SerializeField] private float _speed = 3;
-
+    [SerializeField] private Vector3 _rotate;
     private void Start()
     {
-        transform.DORotate(new Vector3(0, 0, 360), _speed,RotateMode.FastBeyond360).SetLoops(-1,LoopType.Restart);
+        transform.DORotate(_rotate, _speed,RotateMode.FastBeyond360).SetLoops(-1,LoopType.Restart);
     }
 }
--- DOTween/TextChangerDOTW.cs
+++ DOTween/TextChangerDOTW.cs
@@ -5,16 +5,16 @@ using UnityEngine.UI;
 public class TextChangerDOTW : MonoBehaviour
 {
     [SerializeField] private Text _text;
-
+    [SerializeField] private float _speed = 3;
     private void Start()
     {
         Sequence sequence = DOTween.Sequence();
 
-        sequence.Append(_text.DOText("Текст Поменен", 3f));
-        sequence.Append(_text.DOText(" Дополнил", 3).SetRelative());
-        sequence.Append(_text.DOColor(Color.black, 3));
-        sequence.Append(_text.DOColor(Color.black, 3));
-        sequence.Append(_text.DOText("Перебор Текста", 3, true, ScrambleMode.All));
+        sequence.Append(_text.DOText("Текст Поменен", _speed));
+        sequence.Append(_text.DOText(" Дополнил", _speed).SetRelative());
+        sequence.Append(_text.DOColor(Color.black, _speed));
+        sequence.Append(_text.DOColor(Color.black, _speed));
+        sequence.Append(_text.DOText("Перебор Текста", _speed, true, ScrambleMode.All));
 
         sequence.SetLoops(-1, LoopType.Yoyo);
     }
